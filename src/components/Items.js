571590962877f3e1662369cdddfa6d589b504b28
Mukhammed <massalimov.mukhammed@mail.ru>
2024-06-25 00:58:17 +0500

    Pass prop to delete contact

--- src/components/Items.js
+++ src/components/Items.js
@@ -1,10 +1,10 @@
 import Item from "./Item";
 
-export default function Items({ contacts }) {
+export default function Items({ contacts, onDeleteContact }) {
    return (
       <div className="items">
          {contacts.map((contact) => (
-            <Item contactObj={contact} key={contact.id} />
+            <Item contactObj={contact} key={contact.id} onDeleteContact={onDeleteContact} />
          ))}
       </div>
    );
