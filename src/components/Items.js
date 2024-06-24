ca5e51d217913c05d2f266e263fa2f82ccafbf28
Mukhammed <massalimov.mukhammed@mail.ru>
2024-06-24 22:47:52 +0500

    Removed initial contacts and got through parent component

--- src/components/Items.js
+++ src/components/Items.js
@@ -1,32 +1,6 @@
 import Item from "./Item";
 
-const initialContacts = [
-   {
-      id: 1,
-      firstName: "Damir",
-      lastName: "Latfullin",
-      image: "https://i.pravatar.cc/48?u=118836",
-      phone: "+77712345678",
-   },
-   {
-      id: 2,
-      firstName: "Aybek",
-      lastName: "Rakhmet",
-      image: "https://i.pravatar.cc/48?u=933372",
-      phone: "+70712345678",
-   },
-   {
-      id: 3,
-      firstName: "Dimash",
-      lastName: "Rakishev",
-      image: "https://i.pravatar.cc/48?u=499476",
-      phone: "+77012645690",
-   },
-];
-
-export default function Items() {
-   const contacts = initialContacts;
-
+export default function Items({ contacts }) {
    return (
       <div className="items">
          {contacts.map((contact) => (
