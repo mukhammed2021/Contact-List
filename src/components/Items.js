cab57a3870ea039a4e5e0469465e2a0d3c200d0f
Mukhammed <massalimov.mukhammed@mail.ru>
2024-06-25 20:49:54 +0500

    Passed props to Item component

--- src/components/Items.js
+++ src/components/Items.js
@@ -1,10 +1,17 @@
 import Item from "./Item";
 
-export default function Items({ contacts, onDeleteContact }) {
+export default function Items({ contacts, onDeleteContact, onEditContact, editContact, onContact }) {
    return (
       <div className="items">
          {contacts.map((contact) => (
-            <Item contactObj={contact} key={contact.id} onDeleteContact={onDeleteContact} />
+            <Item
+               contactObj={contact}
+               key={contact.id}
+               onDeleteContact={onDeleteContact}
+               onEditContact={onEditContact}
+               editContact={editContact}
+               onContact={onContact}
+            />
          ))}
       </div>
    );
