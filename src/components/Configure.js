e0c3966524e5167114ecb28aec868fc7d42dd293
Mukhammed <massalimov.mukhammed@mail.ru>
2024-06-25 20:52:53 +0500

    Add interactive to button "Edit"

--- src/components/Configure.js
+++ src/components/Configure.js
@@ -1,7 +1,7 @@
-export default function Configure({ onDeleteContact }) {
+export default function Configure({ onDeleteContact, onEditContact }) {
    return (
       <div className="configure">
-         <button type="button" className="configure__btn configure__btn--edit">
+         <button type="button" onClick={onEditContact} className="configure__btn configure__btn--edit">
             Edit
          </button>
          <button type="button" onClick={onDeleteContact} className="configure__btn configure__btn--delete">
